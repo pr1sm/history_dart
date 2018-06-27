be6312c2c1d830a4b5f0491df28b890bbbe1706f
Srinivas Dhanwada <dhanwads@gmail.com>
2018-06-27 14:47:42 -0500

    Document Location
    This commit adds documentation comments to the Location class.

--- lib/src/location.dart
+++ lib/src/location.dart
@@ -1,24 +1,50 @@
-import 'package:quiver/core.dart';
+import 'package:quiver/core.dart' show hashObjects;
 
+/// Abstraction of Location class
+/// 
+/// This class is a subset of the HTML5 Location class that allows 
+/// support of a Location class without relying on the browser.
 class Location {
+  /// The path fragment of the [Location]
+  /// 
+  /// This is the initial portion of the [Location] that starts with a '/' and 
+  /// contains the rest of the location until the [hash] or [search] portions start
   final String pathname;
+
+  /// The hash fragment of the [Location]
+  /// 
+  /// This is the portion of the [Location] that starts with a '#' and 
+  /// contains the rest of the location until the [search] portion starts.
   final String hash;
+
+  /// A unique string representing this [Location]
   final String key;
+
+  /// The search fragment of the [Location]
+  /// 
+  /// This is the portion of the [Location] that starts with a '?' and 
+  /// contains a string of query parameters.
   final String search;
+
+  /// Extra data associated with the [Location] that does not reside in the String representation
+  /// 
+  /// This object can contain any extraneous information not contained in the other variables. This can 
+  /// be used to pass data along with the [Location] that may be too complex to represent as a string.
   final dynamic state;
 
+  /// Construct a [Location].
+  /// 
+  /// This constructor allows partial [Location] construction, providing default values for the rest.
   Location(
-      {String pathname = '/',
-      String hash = '',
-      String key = null,
-      String search = '',
-      dynamic state = null})
-      : this.pathname = pathname,
-        this.hash = hash,
-        this.key = key,
-        this.search = search,
-        this.state = state;
+      {this.pathname = '/',
+      this.hash = '',
+      this.key = null,
+      this.search = '',
+      this.state = null});
 
+  /// Construct a copy of an existing [Location].
+  /// 
+  /// This constructor also allows overriding of individual components, if necessary.
   Location.copy(
     Location other, {
     String pathname,
@@ -27,18 +53,24 @@ class Location {
     String search,
     dynamic state,
   })
-      : this.pathname = pathname ?? other.pathname,
-        this.hash = hash ?? other.hash,
-        this.key = key ?? other.key,
-        this.search = search ?? other.search,
-        this.state = state ?? other.state;
+      : this(
+            pathname: pathname ?? other.pathname,
+            hash: hash ?? other.hash,
+            key: key ?? other.key,
+            search: search ?? other.search,
+            state: state ?? other.state);
 
+  /// Construct a [Location] using an existing map
+  /// 
+  /// This is convenience constructor that allows [Location] information stored in a map to 
+  /// be converted to typed information.
   Location.fromMap(Map<String, dynamic> map)
-      : this.pathname = map['pathname'],
-        this.hash = map['hash'],
-        this.key = map['key'],
-        this.search = map['search'],
-        this.state = map['state'];
+      : this(
+            pathname: map['pathname'],
+            hash: map['hash'],
+            key: map['key'],
+            search: map['search'],
+            state: map['state']);
 
   @override
   bool operator ==(l) => l is Location &&
