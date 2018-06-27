b103d07cd0e07e9b03b9d22d1711c886c5aac5e1
Srinivas Dhanwada <dhanwads@gmail.com>
2018-06-27 01:17:50 -0500

    Add Initial Transition Manager
    This commit adds the initial transition manager stream that will
    handle tracking and notifying listeners of transition changes.
    
    Changes to this class will likely need to be made after this class
    is used in the implementation of the history class variants.

---
+++ lib/src/transition_manager.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'location.dart';
+import 'utils/dom_utils.dart' show ConfirmationCallback;
+
+typedef void ReliqueshPrompt();
+
+class TransitionManager<T> extends Stream<T> {
+  dynamic _prompt = null;
+
+  StreamController<T> _controller = new StreamController<T>.broadcast();
+
+  dynamic get prompt => _prompt;
+
+  ReliqueshPrompt setPrompt(nextPrompt) {
+    if (_prompt != null) {
+      print('WARNING: A history supports only one prompt at a time');
+    }
+
+    _prompt = nextPrompt;
+
+    return () {
+      if(_prompt == nextPrompt) {
+        _prompt = null;
+      }
+    };
+  }
+
+  confirmTransitionTo(Location location, dynamic action, void getConfirmation(result, ConfirmationCallback callback), ConfirmationCallback callback) {
+    if (_prompt != null) {
+      final result = (_prompt is Function) ? prompt(location, action) : prompt;
+
+      if(result is String) {
+        getConfirmation(result, callback);
+      } else {
+        callback(result != false);
+      }
+    } else {
+      callback(true);
+    }
+  }
+
+  @override
+  StreamSubscription<T> listen(void onData(T transition),
+    {Function onError, void onDone(), bool cancelOnError}) =>
+      _controller.stream.listen(onData, onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+  
+  void notify(T transition) {
+    _controller.add(transition);
+  }
+}
