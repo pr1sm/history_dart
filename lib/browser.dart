6737327a8b28d0b13130cb6d2a9340a11fbf328f
Srinivas Dhanwada <dhanwads@gmail.com>
2018-06-29 02:16:42 -0500

    Add Top-Level library definitions

---
+++ lib/browser.dart
@@ -0,0 +1,6 @@
+library history.browser;
+
+export 'package:history/src/history.dart' show History;
+export 'package:history/src/hash_history.dart' show HashHistory;
+export 'package:history/src/browser_history.dart' show BrowserHistory;
+export 'package:history/src/location.dart' show Location;
---
+++ lib/vm.dart
@@ -0,0 +1,5 @@
+library history.vm;
+
+export 'package:history/src/history.dart' show History;
+export 'package:history/src/location.dart' show Location;
+export 'package:history/src/memory_history.dart' show MemoryHistory;
