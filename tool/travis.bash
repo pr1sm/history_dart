1865bcca54ce252a1a958db53091923d862abcd3
Srinivas Dhanwada <dhanwads@gmail.com>
2018-07-05 00:06:29 -0500

    Attempt to Fix Coverage from CI
    This commit attempts to fix coverage reports generating properly in CI
    by running the coverage task using xvfb.

--- tool/travis.bash
+++ tool/travis.bash
@@ -4,4 +4,4 @@ pub get --packages-dir
 pub run dart_dev format --check && \
 pub run dart_dev analyze && \
 pub run dart_dev test -p vm -p chrome && \
-DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked pub run dart_dev coverage --no-html
+DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked xvfb-run -s '-screen 0 1024x768x24' pub run dart_dev coverage --no-html
