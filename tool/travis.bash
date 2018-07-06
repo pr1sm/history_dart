3f2b45909e808d5509af036e3d42d336e7048506
Srinivas Dhanwada <dhanwads@gmail.com>
2018-07-05 23:47:41 -0500

    Adjust CI Script
    This commit changes the CI script to properly run xvfb when in travis
    environment and not run xvfb when run locally (macOS)

--- tool/travis.bash
+++ tool/travis.bash
@@ -4,4 +4,8 @@ pub get --packages-dir
 pub run dart_dev format --check && \
 pub run dart_dev analyze && \
 pub run dart_dev test -p vm -p chrome && \
-DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked xvfb-run -s '-screen 0 1024x768x24' pub run dart_dev coverage --no-html
+if [ "$TEST_ENV" = "travis"]; then
+  DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked xvfb-run -s '-screen 0 1024x768x24' pub run dart_dev coverage --no-html
+else
+  DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked pub run dart_dev coverage --no-html
+fi
