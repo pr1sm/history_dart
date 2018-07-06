ebeb973ce98948b7988e9038d5e14820d65c83e9
Srinivas Dhanwada <dhanwads@gmail.com>
2018-07-06 00:00:03 -0500

    Fix CI Environment Setup
    This commit fixes a bug due to attempting to add an Environment Variable
    branch to run xvfb during Travis CI runs instead of locally. This was
    fixed by defining the environment variable on the specific command and
    fixing a syntax error in the bash script.

--- tool/travis.bash
+++ tool/travis.bash
@@ -4,7 +4,7 @@ pub get --packages-dir
 pub run dart_dev format --check && \
 pub run dart_dev analyze && \
 pub run dart_dev test -p vm -p chrome && \
-if [ "$TEST_ENV" = "travis"]; then
+if [ "$TEST_ENV" = "travis" ]; then
   DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked xvfb-run -s '-screen 0 1024x768x24' pub run dart_dev coverage --no-html
 else
   DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked pub run dart_dev coverage --no-html
