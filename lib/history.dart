6862b28505fad32d8bcb9e15d454fedd90ca0c91
Srinivas Dhanwada <dhanwads@gmail.com>
2018-07-13 23:40:03 -0500

    Format
    <--------------------Commit (72 char wrapped lines)-------------------->
    
    Ticket/Issue:

--- lib/history.dart
+++ lib/history.dart
@@ -27,4 +27,5 @@ export 'package:history/src/browser/browser_history.dart' show BrowserHistory;
 export 'package:history/src/core/history.dart' show History;
 export 'package:history/src/core/memory_history.dart' show MemoryHistory;
 export 'package:history/src/core/location.dart' show Location;
-export 'package:history/src/utils/utils.dart' show Action, Confirmation, HashType, Prompt;
+export 'package:history/src/utils/utils.dart'
+    show Action, Confirmation, HashType, Prompt;
