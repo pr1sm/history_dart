6f26903b9e553deac854ea2157c4b81fc37f1bdf
Srinivas Dhanwada <dhanwads@gmail.com>
2018-07-13 22:27:24 -0500

    Remove Browser Only Lib
    This commit removes the browser only import since the only difference between
    this and the full history import is the exclusion of the Memory History. There is
    no good reason to exclude Memory History from the browser, so using the 
    history import is all that is needed.

--- lib/browser.dart
+++
@@ -1,6 +0,0 @@
-library history.browser;
-
-export 'package:history/src/browser/hash_history.dart' show HashHistory;
-export 'package:history/src/browser/browser_history.dart' show BrowserHistory;
-export 'package:history/src/core/history.dart' show History;
-export 'package:history/src/core/location.dart' show Location;
