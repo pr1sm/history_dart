7028cb949634a6bca0bde469e5571dde8d1e56ac
Srinivas Dhanwada <dhanwads@gmail.com>
2018-07-13 22:58:33 -0500

    Add Utils Definitions to exports
    This commit adds more exports to the library files. This includes all of the 
    typedefs that exist in the public api.

--- lib/history.dart
+++ lib/history.dart
@@ -5,3 +5,4 @@ export 'package:history/src/browser/browser_history.dart' show BrowserHistory;
 export 'package:history/src/core/history.dart' show History;
 export 'package:history/src/core/memory_history.dart' show MemoryHistory;
 export 'package:history/src/core/location.dart' show Location;
+export 'package:history/src/utils/utils.dart' show Action, Confirmation, HashType, Prompt;
