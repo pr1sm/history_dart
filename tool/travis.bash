91077fe97d0a2b1acb2aac40a86d06c49402c206
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-10-08 03:37:09 -0500

    Update Travis Script
    This commit updates the travis ci bash script to work without dart_dev.
    This is a temporary script to get things going -- more changes are
    necessary and will be made!

--- tool/travis.bash
+++ tool/travis.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd "$(git rev-parse --show-toplevel)"
-pub get --packages-dir
-pub run dart_dev format --check && \
-pub run dart_dev analyze && \
-pub run dart_dev test -p vm -p chrome && \
-if [ "$TEST_ENV" = "travis" ]; then
-  DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked xvfb-run -s '-screen 0 1024x768x24' pub run dart_dev coverage --no-html
-else
-  DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked pub run dart_dev coverage --no-html
-fi
+pub get
+dartanalyzer . # TODO: move this back into success check
+pub run dart_style:format -n test/ lib/ example/ && \
+[[ "$(pub run dart_style:format -n test/ lib/ example/ | grep -c .dart)" -eq "0" ]] && \ # TODO: Simplify this!
+pub run test -p "chrome,vm" test/unit/browser_test_runner.dart test/unit/core_test_runner.dart # TODO: add option to specify test runner files
+# TODO: add converage back
+# if [ "$TEST_ENV" = "travis" ]; then
+#   DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked xvfb-run -s '-screen 0 1024x768x24' pub run dart_dev coverage --no-html
+# else
+#   DARTIUM_EXPIRATION_TIME="1577836800" DART_FLAGS=--checked pub run dart_dev coverage --no-html
+# fi
